2460e10409da94b66cc999e9e8c2c9135353a144
Ubuntu <ubuntu@ip-172-31-7-157.us-east-2.compute.internal>
2018-09-16 17:03:43 +0000

    Install control plane including BOSH director using Bosh Bootloader

--- bbl/loadbosh_using_bbl.sh
+++ bbl/loadbosh_using_bbl.sh
@@ -1,1 +1,5 @@
-bbl up     --aws-access-key-id AKIAJ4ESGQX7P7GISLEQ     --aws-secret-access-key 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     --aws-region us-east-2     --iaas aws
+bbl up \
+    --aws-access-key-id $AWS_ACCESS_KEY \
+    --aws-secret-access-key $AWS_SECRET_ACCESS_KEY \
+    --aws-region $AWS_REGION \
+    --iaas aws
